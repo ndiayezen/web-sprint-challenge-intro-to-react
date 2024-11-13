3d4acb30a5c7518e8405bfd7ded5a7a279aa1b0d
Ndiaye Kane <ndiayezen@live.fr>
2024-11-13 14:26:25 -0500

    finished

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
+
+
 function App() {
-  // ❗ Create state to hold the data from the API
+  // ❗ Create state to hold the data from the API 
   // ❗ Create effects to fetch the data and put it in state
+  const [characters, setCharacters] = useState([])
+  const getData = async () => {
+    const {data: planets} = await axios.get (urlPlanets);
+    const {data: people} = await axios.get (urlPeople);
+    const data = people.map(person => {
+      return {name: person.name, planet: planets.find(planet => planet.id === person.homeworld).name}
+    })
+    setCharacters(data)
+
+  }
+
+  useEffect (() => {
+    getData();
+  }, [])
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {characters.map(character => <Character name = {character.name} planet = {character.planet} key= {character.name}/>)}
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Character() { // ❗ Add the props
+function Character({name, planet}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+
+ 
+
+
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+
+  const [showPlanet, setShowPlanet] = useState(false)
+
   return (
-    <div>
+    <div className = "character-card" onClick = {()=> setShowPlanet(!showPlanet)}>
       {/* Use the same markup with the same attributes as in the mock */}
+    
+        <h3 className = "character-name">{name}</h3>
+        {showPlanet && <p>Planet: <span className="character-planet">{planet}</span></p>}
+      
     </div>
   )
 }
